0f88bab9891ea0667e734cfc10a7d9dfdb398ea6
domi-exe <dominikadolik6@gmail.com>
2019-09-13 13:05:43 +0200

    post, header
    header: updated header (an image as background), changed colors
    post: post view, state block
    TODO: fix ShowDetails function

--- frontend/src/menu.js
+++ frontend/src/menu.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MenuList(props) {
     return (
         <ul>
-            { props.menu.map((item) => <li><a key={ item } href="\">{ item }</a></li>) }
+            { props.menu.map((item) => <li><a key={ item } href={item} id={item}>{ item }</a></li>) }
         </ul>
     );
 }
--- frontend/src/post.js
+++ frontend/src/post.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
 require('./post.css');
 
+function ShowDetails(props) {
+  const published_date = props.published_date;
+  const created_date = props.created_date;
+  const author = props.author;
+
+  if (published_date) {
+    return(
+      <h3>Author: {author}, created: {created_date}, published: {published_date}</h3>
+    );
+  }
+  return(
+    <h3>Author: {author}, created: {created_date}</h3>
+  );
+}
+
 class Post extends Component {
     constructor(props) {
         super(props);
-        this.author = 'Dominika';
-        //this.title = 'Title';
-        this.text = 'abc';
-        //this.created_date = '23.04.2019';
-        //this.published_date = '23.04.2019';
+        this.state = {
+          author: 'Dominika',
+          title: 'Taki piekny tytul ze szog',
+          text: 'Jakis tam se tekst jest elo',
+          created_date: '23.04.2019',
+          published_date: '23.04.2019',
+        }
     };
 
     render() {
         return (
-            <span id='Post'>
+            <div id='Post'>
                 <table>
+                    <thead>
+                        <th id='PostHeader'>
+                        <div id='PostTitle'>
+                          <a href='Post'><h1>{ this.state.title }</h1></a>
+                        </div>
+                        <div id='PostAuthorDate'>
+                            <ShowDetails author={ this.state.author}
+                              created={this.state.created_date}
+                              published={this.state.published_date}
+                            />
+                          </div>
+                        </th>
+                    </thead>
+                    <tbody>
                     <tr>
-                        <p>{this.author}</p>
+                        <div id='PostText'>
+                          <p>{ this.state.text }</p>
+                        </div>
                     </tr>
-                    <tr>
-                        <p>{this.text}</p>
-                    </tr>
+                    </tbody>
                 </table>
-            </span>
+            </div>
         );
     }
 }

ac75f20e7d457938f635d274680470d50dcbe890
domi-exe <dominikadolik6@gmail.com>
2019-10-21 11:40:52 +0200

    removed dir
    Removed unused installer directory

--- installer/api_packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script to install packages/settings used in REST development
-
-pip install pep8 autopep8 pylint djangorestframework markdown django-filter
--- installer/createvenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Bash script for virtual environment named: venv
-# Create, run, upgrade and install requirements.txt
-
-cd ../backend
-python3 -m venv venv
-source venv/bin/activate
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
